498bdbce99a5547e9a2d634c986f6374be11796a
zadnap <88957683+zadnap@users.noreply.github.com>
2026-05-06 09:40:31 +0700

    fix(env): add conditional render of analysis

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { Outlet } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/react';
 import { AuthProvider } from './contexts/AuthContext';
 import { PopupProvider } from './contexts/PopupContext';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [Analytics, setAnalytics] = useState(null);
+
+  useEffect(() => {
+    if (import.meta.env.VITE_ENABLE_ANALYTICS === 'true') {
+      import('@vercel/analytics/react').then((mod) => {
+        setAnalytics(() => mod.Analytics);
+      });
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <PopupProvider>
         <Outlet />
-        <Analytics />
+        {Analytics && <Analytics />}
       </PopupProvider>
     </AuthProvider>
   );
